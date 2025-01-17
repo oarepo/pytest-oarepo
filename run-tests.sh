efe73c7f99c407a96eef1fc8fcea6360d4bee746
Ronald Krist <ronald.krist@cesnet.cz>
2025-01-17 14:12:09 +0100

    tests controlling importability

---
+++ run-tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+OAREPO_VERSION=${OAREPO_VERSION:-12}
+PYTHON="${PYTHON:-python3.12}"
+VENV=".venv"
+
+test_scripts() {
+  local folder="$1/*"
+  for entry in $folder
+  do
+    if [[ "$entry" == *.py ]]
+    then
+      module="$(echo "$entry" | sed -e 's|^\./||' -e 's|/|.|g' -e 's|\.py$||')"
+      echo $module
+      python -c "import $module"
+    fi
+  done
+}
+
+"${PYTHON}" -m venv $VENV
+. $VENV/bin/activate
+pip install -U setuptools pip wheel
+pip install -e .
+pip install "oarepo[tests]==${OAREPO_VERSION}.*" #correct version
+pip install deepmerge
+
+test_scripts "./pytest_oarepo"
+
+pip install oarepo-requests
+test_scripts "./pytest_oarepo/requests"
+
+pip install oarepo-communities
+test_scripts "./pytest_oarepo/communities"
