78516bcb73bfd015c2b151f7f9596593026c8df9
Ronald Krist <ronald.krist@cesnet.cz>
2025-06-12 12:19:01 +0200

    edited users

--- pytest_oarepo/users.py
+++ pytest_oarepo/users.py
@@ -9,28 +9,45 @@ def users(app, db, UserFixture):
     """
     user1 = UserFixture(
         email="user1@example.org",
-        password="password",
+        password="password", # NOSONAR
         active=True,
         confirmed=True,
+        user_profile={
+            "full_name": "user 1",
+            "affiliations": "cesnet",
+        },
+        preferences={
+            "locale": "en"
+        }
     )
     user1.create(app, db)
 
     user2 = UserFixture(
         email="user2@example.org",
-        password="beetlesmasher",
+        password="beetlesmasher", # NOSONAR
         username="beetlesmasher",
         active=True,
         confirmed=True,
+        user_profile={
+            "full_name": "beetlesmasher",
+            "affiliations": "CERN",
+        },
+        preferences={
+            "locale": "en"
+        }
     )
     user2.create(app, db)
 
     user3 = UserFixture(
         email="user3@example.org",
-        password="beetlesmasher",
+        password="beetlesmasher",  # NOSONAR
         username="beetlesmasherXXL",
         user_profile={
             "full_name": "Maxipes Fik",
-            "affiliations": "CERN",
+            "affiliations": "cesnet",
+        },
+        preferences={
+            "locale": "cs"
         },
         active=True,
         confirmed=True,
@@ -39,10 +56,10 @@ def users(app, db, UserFixture):
 
     user4 = UserFixture(
         email="user4@example.org",
-        password="african",
+        password="african",  # NOSONAR
         username="african",
         preferences={
-            "timezone": "Africa/Dakar", # something without daylight saving time; +0.0
+            "timezone": "Africa/Dakar"  # something without daylight saving time; +0.0
         },
         active=True,
         confirmed=True,
@@ -51,16 +68,41 @@ def users(app, db, UserFixture):
 
     user5 = UserFixture(
         email="user5@example.org",
-        password="mexican",
+        password="mexican",  # NOSONAR
         username="mexican",
         preferences={
-            "timezone": "America/Mexico_City", # something without daylight saving time
+            "timezone": "America/Mexico_City"  # something without daylight saving time
         },
         active=True,
         confirmed=True,
     )
     user5.create(app, db)
 
+
+    user6 = UserFixture(
+        email="user6@example.org",
+        password="password",  # NOSONAR
+        active=True,
+        confirmed=True,
+    )
+    user6.create(app, db)
+
+    user7 = UserFixture(
+        email="user7@example.org",
+        password="password",  # NOSONAR
+        active=True,
+        confirmed=True,
+    )
+    user7.create(app, db)
+
+    user10 = UserFixture(
+        email="user10@example.org",
+        password="password",  # NOSONAR
+        active=True,
+        confirmed=True,
+    )
+    user10.create(app, db)
+
     db.session.commit()
     _index_users()
-    return [user1, user2, user3, user4, user5]
+    return [user1, user2, user3, user4, user5, user6, user7, user10]
