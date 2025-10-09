5a5c33a50e49756a1c5eb8e59e38f740850d30c8
ron <kristron6@gmail.com>
2025-10-09 09:59:55 +0200

    fake test module

---
+++ tests/__init__.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of pytest-oarepo (see https://github.com/oarepo/pytest_oarepo).
+#
+# pytest-oarepo is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""Test package for pytest-oarepo.
+
+This package contains tests for the pytest-oarepo library.
+"""
+
+from __future__ import annotations
